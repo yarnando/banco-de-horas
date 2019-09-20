244d9123badd3dc4d5d0ee421911d9e580e6ab87
yarnando <fernandosilva.smsrio@gmail.com>
2019-09-19 21:15:00 -0300

    Styiling

--- src/components/shared/card/index.js
+++ src/components/shared/card/index.js
@@ -3,17 +3,19 @@ import './styles.css'
 
 const Card = (props) => (
     <div className="card">
-        {props.withHeader && 
-        <div className="card__header">
-            {props.withHeader}
-        </div>}
-        <div className="card__content">
-            {props.children}
+        <div className="card__inner">
+            {props.withHeader && 
+                <div className="card__header">
+                    {props.withHeader}
+                </div>}
+                <div className="card__content">
+                    {props.children}
+                </div>
+                {props.withFooter && 
+                <div className="card__footer">
+                    {props.withFooter}
+                </div>}    
         </div>
-        {props.withFooter && 
-        <div className="card__footer">
-            {props.withFooter}
-        </div>}
     </div>    
 );
 
