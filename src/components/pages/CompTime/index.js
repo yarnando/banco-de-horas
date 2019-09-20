f367cfd5297a7fe3ef6837e5b2ffc627f27a7d6d
yarnando <fernandosilva.smsrio@gmail.com>
2019-09-19 21:17:58 -0300

    Result refactoring

--- src/components/pages/CompTime/index.js
+++ src/components/pages/CompTime/index.js
@@ -1,44 +1,23 @@
 import React, { Component } from "react";
 
-import { connect } from "react-redux";
-
-import Menu from './components/Menu'
 import HoursOfDayForm from "./components/HoursOfDayForm";
+import Menu from './components/Menu'
+import Result from './components/Result'
 import List from "./components/List";
 
 class CompTime extends Component {
-
-    state = {
-        hoursBank: {
-            hours: 0,
-            minutes: 0
-        }
-    }
 
   render() {
     return (
       <section className="container">
         <HoursOfDayForm />
-
         <Menu/>
-        <div className="row">
-            <div className="grid-item-6 space-around">
-                <div>
-                    Banco de horas total: {this.props.hoursBank.hours || '00'}:{this.props.hoursBank.minutes || '00'}
-                </div>
-            </div>
-        </div>
+        <Result/>
         <List/>
       </section>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  hoursBank: state.comptime.hoursBank,
-});
 
-
-export default connect(
-  mapStateToProps,
-)(CompTime);
+export default CompTime
