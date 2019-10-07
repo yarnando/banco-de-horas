c88a8f004953f7f6a301bcff2fe87307ee798483
yarnando <fernandosilva.smsrio@gmail.com>
2019-10-07 17:09:23 -0300

    Login/signup validation

--- src/components/pages/Auth/Signin/index.js
+++ src/components/pages/Auth/Signin/index.js
@@ -1,7 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { creators as authActions } from '../../../../store/ducks/auth'
 
-// import { Container } from './styles';
+import Loading from '../../../shared/loading'
 
-const Signin = () => <div>Signin</div>;
+class Signin extends Component {
 
-export default Signin;
+    state = {
+        passwordError: "",
+        emailError: ""
+    }
+
+    handleInputChange = (e, field) => {
+        const { user } = this.props.auth
+        user[field] = e.target.value
+        this.props.user(user)
+        if(field == 'email') this.validateEmail()
+        if(field == 'password') this.validatePassword()
+    }
+
+    validateEmail = () => {
+        let email = this.props.auth.user.email
+        console.log(`testando o email: ${email}`)
+        if(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            console.log('email ok')
+            this.setState({ emailError: "" })
+            return true
+        } else {
+            this.setState({ emailError: "Por favor, digite um e-mail válido." })
+            return false
+        }
+    }
+    validatePassword = () => {
+        let password = this.props.auth.user.password
+        if(password.length >= 6) {
+            this.setState({ passwordError: "" })
+            return true
+        } else {
+            this.setState({ passwordError: "A senha deve ter, pelo menos, 6 caracteres." })
+            return false
+        }        
+    }
+
+    validateAndSignIn = (e) => {
+        e.preventDefault()
+        if(this.validateEmail() ) return false
+        if(this.validatePassword() ) return false
+        this.props.signIn()
+    }
+
+    render() {
+        return <div className="flex flex-1 content-center align-center">
+            <div className="container container-sm mb-4">
+                <h1 className="text-center pt-4">Login</h1>
+                <form noValidate onSubmit={(e) => this.validateAndSignIn(e)}>
+                    <div className="row">
+                        <div className="grid-item-6">
+                            <div className="input-box">
+                                <label>Email</label>
+                                <input value={this.props.auth.user.email} onChange={(e) => this.handleInputChange(e, 'email')}/>
+                                { this.state.emailError && <span className="errorMsg">{this.state.emailError}</span>}
+                            </div>                             
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="grid-item-6">
+                            <div className="input-box">
+                                <label>Senha</label>
+                                <input type="password" value={this.props.auth.user.password} onChange={(e) => this.handleInputChange(e, 'password')}/>
+                                { this.state.passwordError && <span className="errorMsg">{this.state.passwordError}</span>}
+                            </div>                             
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="grid-item-6">
+                                {this.props.loading ? <Loading loading={this.props.loading} size={22}/> : <button className="primary">Fazer Login</button>}     
+                                <button type="button" onClick={() => this.props.history.push("/signup")}>Não possuo cadastro</button>                     
+                        </div>
+                    </div>
+                </form>    
+            </div>
+        </div>
+    }
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(authActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Signin)
